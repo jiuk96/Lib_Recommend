e8436727a1c514aab695c061c9052bd7bf1f9096
jiuk <jiuk96@naver.com>
2021-12-05 21:55:54 +0900

    로컬 이름 db수정

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.register_blueprint(board)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://gowoon:gowoon@127.0.0.1:3306/lib"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/library_recommend"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-#app.secret_key = 'asodfajsdofijac'
+app.secret_key = 'asodfajsdofijac'
 
 db.init_app(app)
 bcrypt = Bcrypt(app)
