c9f4dc33a2d57255dca8c8d2a29836ff88035eed
jiuk <jiuk96@naver.com>
2021-12-08 19:02:49 +0900

    30분마다 코드 자동실행하여 테이블 정보 최신화가능

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # 파일 실행, DB 위치 관련 파일
 
 # created by 장지욱 11.09
+# modified by 장지욱 12.06 apscheduler 모듈을 사용해서 매 30분마다 코드 자동실행하여 Seat 테이블 정보 최신화해주기
 
-# import time
 import pymysql
 from flask import Flask
 from api import *
 from db_connect import db
 from flask_bcrypt import Bcrypt
-# from apscheduler.schedulers.background import BackgroundScheduler
-# import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
 
 app = Flask(__name__)
 app.register_blueprint(board)
@@ -21,23 +21,17 @@ app.secret_key = 'asodfajsdofijac'
 db.init_app(app)
 bcrypt = Bcrypt(app)
 
-# with app.app_context():
-#     db.create_all()
+sched = BackgroundScheduler(daemon=True)
+sched.start()
 
-# sched = BackgroundScheduler(daemon=True)
-# sched.start()
+# 매 30분마다 코드 자동실행하여 Seat 테이블 정보 최신화해주기 (07:00~22:30분까지 30분 주기로 체크)
+@sched.scheduled_job('cron', hour='7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22', minute='30,00', id='test_2')
+def seat_update_every_30minutes():
+    with app.app_context():
+        seat_update()
 
-# @sched.scheduled_job('interval', seconds=20, id='test_1')
-# def job1():
-#     print(f'job1 : {time.strftime("%H:%M:%S")}')
-    # seat_update()
-
-# @sched.scheduled_job('cron', hour='12,13,14,15,16,17', minute='48', id='test_2')
-# def seat_update_every_30minutes():
-#     seat_update()
-
-
-# atexit.register(lambda: sched.shutdown(wait=False))
+# 도중에 홈페이지 꺼지면 끝
+atexit.register(lambda: sched.shutdown(wait=False))
 
 if __name__ == '__main__':
     app.run(debug=True)
