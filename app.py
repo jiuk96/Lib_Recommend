806bbc7bf76274bca83e7cb7cff165313908ad99
jiuk <jiuk96@naver.com>
2021-12-10 17:14:58 +0900

    interval을 통한 10초마다 실행

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 # created by 장지욱 11.09
 # modified by 장지욱 12.06 apscheduler 모듈을 사용해서 매 30분마다 코드 자동실행하여 Seat 테이블 정보 최신화해주기
-
+import time
 import pymysql
 from flask import Flask
 from api import *
@@ -29,6 +29,11 @@ sched.start()
 def seat_initailze_atnight():
     with app.app_context():
         seat_initialize()
+
+# # 5초마다 실행
+# @sched.scheduled_job('interval', seconds=10, id='test_1')
+# def job1():
+#     print(f'job1 : {time.strftime("%H:%M:%S")}')
 
 # 매 30분마다 코드 자동실행하여 Seat 테이블 정보 최신화해주기 (07:00~22:30분까지 30분 주기로 체크)
 @sched.scheduled_job('cron', hour='7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22', minute='30,00', id='test_2')
