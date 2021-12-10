d13d0de5f2aff21e92f6fb1c5fd003ba3c6c1286
jiuk <jiuk96@naver.com>
2021-12-10 18:37:09 +0900

    seat 알고리즘 통합 및 수정사항

--- api.py
+++ api.py
@@ -165,13 +165,8 @@ def reserve():
         if request.method == 'GET':
             data = Seat.query.filter(Seat.used == 1).all() #지금 사용중인 좌석 정보
             preference_info = db.session.query(User.distance,User.acheater,User.windowfar,User.door).filter(User.user_id == session['login']).first()
-            data.append(prefer(preference_info))
-            #distance = User.query.filter(
-            #windowfar = User.query.filter(
-            #acheater = User.query.filter(
-            #door = User.query.filter(
-            #
-            # 좌석 추천 알고리즘 부분(각 선호 정보가 == 1이면 조건문 실행하도록 변경)
+            preference_seat = recommend(preference_info[0],preference_info[1],preference_info[2],preference_info[3])
+            data.append(preference_seat)
             return render_template('reserve.html', seat_list = data) # seat_list 현재 좌석 정보 넘겨주기 + seat_list[-1]에 알고리즘을 통한 추천좌석까지 보내줌
         else:
             now = datetime.now()
@@ -180,10 +175,6 @@ def reserve():
             reserved_time = now
             starttime = datetime.strptime(request.form['starttime'], '%Y/%m/%d %H:%M')
             finishtime = datetime.strptime(request.form['finishtime'], '%Y/%m/%d %H:%M')
-
-            #사용 시간은 무조건 지금보다는 앞에 해야한다.
-            if starttime < now: 
-                return jsonify({"result":"NoReserve"}) # 사용시간이 지금 시각보다 늦은경우
 
             #끝나는 시간이 시작시간보다 더 앞이면 알람경고
             if starttime > finishtime: 
@@ -281,3 +272,62 @@ def seat_update():
             db.session.commit()
 
     return "success"
+
+def recommend(distance, acheater, windowfar, door): # 좌석 추천 알고리즘
+    reclist = [x for x in range(1, 25)]
+    seatlist = db.session.query(Seat.used).all()
+    
+    if distance: # Prefer 클래스의 distance가 True면 조건문이 실행됨
+        for i in range(1, 25):
+            if (i==1) and (seatlist[i-1][0] == True):
+                for k in [i, i+1, i+4, i+5]:
+                    if k in reclist: reclist.remove(k)
+
+            if (i==2 or i==3) and (seatlist[i-1][0] == True):
+                for k in [i-1, i, i+1, i+3, i+4, i+5]:
+                    if k in reclist: reclist.remove(k)
+
+            if (i==4) and (seatlist[i-1][0] == True):
+                for k in [i-1, i, i+3, i+4]:
+                    if k in reclist: reclist.remove(k)
+
+            if (i==5 or i==9 or i==13 or i==17) and (seatlist[i-1][0] == True):
+                for k in [i-4, i-3, i, i+1, i+4, i+5]:
+                    if k in reclist: reclist.remove(k)
+
+            if (i==6 or i==7 or i==10 or i==11 or i==14 or i==15 or i==18 or i==19) and (seatlist[i-1][0] == True):
+                for k in [i-5, i-4, i-3, i-1, i, i+1, i+3, i+4, i+5]:
+                    if k in reclist: reclist.remove(k)
+
+            if (i==8 or i==12 or i==16 or i==20) and (seatlist[i-1][0] == True):
+                for k in [i-5, i-4, i-1, i, i+3, i+4]:
+                    if k in reclist: reclist.remove(k)
+                    
+            if (i==21) and (seatlist[i-1][0] == True):
+                for k in [i-4, i-3, i+1]:
+                    if k in reclist: reclist.remove(k)
+                
+            if (i==22 or i==23) and (seatlist[i-1][0] == True):
+                for k in [i-5, i-4, i-3, i-1, i, i+1]:
+                    if k in reclist: reclist.remove(k)
+
+            if (i==24) and (seatlist[i-1][0] == True):
+                for k in [i-5, i-4, i-1, i]:
+                    if k in reclist: reclist.remove(k)
+                
+    if acheater:
+        for k in [19, 20, 23, 24]:
+            if k in reclist:
+                reclist.remove(k)
+
+    if windowfar:
+        for k in [1, 2, 3, 4, 8, 12, 16, 20, 24]:
+            if k in reclist:
+                reclist.remove(k)
+        
+    if door:
+        for k in [17, 18, 21, 22]:
+            if k in reclist:
+                reclist.remove(k)
+
+    return reclist # 추천하는 seat의 num 리스트를 return
