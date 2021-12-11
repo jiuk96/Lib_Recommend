d051800a36e286765b32d059100c9526f4c27636
jiuk <jiuk96@naver.com>
2021-12-12 02:17:18 +0900

    instance 2개로 설정 후 sec = 5로 변경

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ import atexit
 app = Flask(__name__)
 app.register_blueprint(board)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/lib_recommend"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/library_recommend"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.secret_key = 'asodfajsdofijac'
 
 db.init_app(app)
 bcrypt = Bcrypt(app)
 
-sched = BackgroundScheduler(daemon=True)
+sched = BackgroundScheduler({'apscheduler.job_defaults.max_instances': 2})
 sched.start()
 
 # 매일 밤 12시가 되면 Seat 테이블 정보를 초기화 해준다. 
@@ -29,18 +29,12 @@ def seat_initailze_atnight():
     with app.app_context():
         seat_initialize()
 
-# 10초마다 코드 자동실행하여 Seat 테이블 정보 최신화해주기
-@sched.scheduled_job('interval', seconds=10, id='test_2')
+# 5초마다 코드 자동실행하여 Seat 테이블 정보 최신화해주기
+@sched.scheduled_job('interval', seconds=5, id='test_2')
 def seat_update_every10sec():
-    #print(f'job1 : {time.strftime("%H:%M:%S")}') #정상 작동하는지 체크
+    # print(f'job1 : {time.strftime("%H:%M:%S")}') #정상 작동하는지 체크
     with app.app_context():
         seat_update()
-
-# 매 30분마다 코드 자동실행하여 Seat 테이블 정보 최신화해주기 (07:00~22:30분까지 30분 주기로 체크)
-#@sched.scheduled_job('cron', hour='7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22', minute='30,00', id='test_2')
-#def seat_update_every_30minutes():
-#    with app.app_context():
-#        seat_update()
 
 # 도중에 홈페이지 꺼지면 끝
 atexit.register(lambda: sched.shutdown(wait=False))
